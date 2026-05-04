c641eef1e4d7d44f03989d1964af1250f3c3ec74
muzi470-lgtm <muzammiln411@gmail.com>
2026-05-04 23:44:19 +0500

    sitemap applied

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://thedailyscale.online',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://thedailyscale.online/bmi', // Agar aapka BMI page hai
+      lastModified: new Date(),
+    },
+  ];
+}
